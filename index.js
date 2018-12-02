1749750295f6e05dbbffdf7cdc51f1b976433ce3
imran <mrimranzahoor@gmail.com>
2018-12-02 22:28:11 +0500

    Removed extra code.

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/** @format */
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-
-
 //import a library to create a component
 import React from 'react';
 import {AppRegistry} from 'react-native';
